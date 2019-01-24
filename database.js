ee237e674b9597e2f9266da9f8ecb893cf311a0a
Brehmly <31944827+Brehmly@users.noreply.github.com>
2019-01-23 23:10:09 -0500

    Homework 3

---
+++ database.js
@@ -0,0 +1,14 @@
+var dataset = [
+  {
+    color:"red",
+    message:"I am a cool message!"
+  },
+  {
+    color:"blue",
+    message:"Living on the planet that is cool and blue as me!"
+  },
+  {
+    color:"green",
+    message:"The grass is always greener"
+  }
+]
